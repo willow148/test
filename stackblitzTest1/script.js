28881675d9fc99970d720830d52bec4b6ed0acf8
willow148 <yuzy90125@yahoo.co.jp>
2025-04-02 14:36:33 +0900

    Updated

--- stackblitzTest1/script.js
+++ stackblitzTest1/script.js
@@ -1,1 +1,12 @@
+
+console.log = (...args) => {
+    for (let arg of args) {
+        // console.logに入力された文字列を改行つきで保持
+        outstr = outstr + arg + "<br>";
+    }
+    // HTMLのid="console"に対して文字列outstrを設定する
+    document.getElementById("console").innerHTML = outstr;
+}
+
+
 console.log('Hello!');
