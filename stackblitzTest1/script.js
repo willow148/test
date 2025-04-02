8af136e5b755621d0d6e0361b5138d321e8e5850
willow148 <yuzy90125@yahoo.co.jp>
2025-04-02 15:02:49 +0900

    Updated

--- stackblitzTest1/script.js
+++ stackblitzTest1/script.js
@@ -1,4 +1,4 @@
-
+/*
 function consoleTest(...args) {
     for (let arg of args) {
         // console.logに入力された文字列を改行つきで保持
@@ -10,3 +10,28 @@ function consoleTest(...args) {
 
 
 consoleTest('Hello!');
+*/
+
+
+        // ページ読み込み時のイベントを登録
+        window.onload = function () {
+            // ブラウザ上に表示する文字列を保持する一時変数
+            let outstr = "";
+            // console.logの挙動
+            console.log = (...args) => {
+                for (let arg of args) {
+                    // console.logに入力された文字列を改行つきで保持
+                    outstr = outstr + arg + "<br>";
+                }
+                // HTMLのid="console"に対して文字列outstrを設定する
+                document.getElementById("console").innerHTML = outstr;
+            }
+
+            // 1～100までを出力するコード
+            const testObj = {kye1:"AAA", key2:12345}
+            console.log(JSON.stringify(testObj));
+            //for (let i = 1; i < 100; i++) {
+            //    console.log(i);
+            //}
+        }
+  
