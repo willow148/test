45985e2e18ae0a43182db4856b5c92c7c7f0aae3
willow148 <yuzy90125@yahoo.co.jp>
2025-04-02 15:39:25 +0900

    Update script.js

--- stackblitzTest1/script.js
+++ stackblitzTest1/script.js
@@ -1,19 +1,19 @@
 
-function consoleTest() {
-    /*
+function consoleTest(...args) {
+    let outstr = "";
     for (let arg of args) {
         // console.logに入力された文字列を改行つきで保持
         outstr = outstr + arg + "<br>";
     }
-    */
+    
     // HTMLのid="console"に対して文字列outstrを設定する
-    document.getElementById("console").innerHTML = "AAA";
+    document.getElementById("console").innerHTML = outstr;
 }
 
 function showMessage() {
     document.getElementById('message').textContent = '外部スクリプトからこんにちは！';
     
-    consoleTest();
+    consoleTest("あああああああああああああああああ");
 }
 
 /*
