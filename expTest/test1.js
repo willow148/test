0c7c857467e003eaca066a63aae9a8a5110cfc0f
willow148 <yuzy90125@yahoo.co.jp>
2025-04-07 11:27:32 +0900

    Update test1.js

--- expTest/test1.js
+++ expTest/test1.js
@@ -1,3 +1,22 @@
-function Alert(msg) {
-  alert(msg);
+
+function consoleLog(arg) {
+  const replacer = (key, value) =>
+    typeof value === "bigint" ? value.toString() : value;
+  
+  const typeval = typeof(arg);
+  if (typeval === "object") {
+    let rtn;
+    try {
+      rtn = JSON.stringify(JSON.decycle(arg), replacer);
+    } catch(e) {
+      if (e instanceof TypeError) {
+        document.getElementById("console").innerHTML += JSON.stringify(e) + "<br />";
+      } else {
+        document.getElementById("console").innerHTML += JSON.stringify(e) + "<br />";
+      }
+    }
+    document.getElementById("console").innerHTML += rtn + "<br />";
+  } else {
+    document.getElementById("console").innerHTML += arg + "<br />";
+  }
 }
